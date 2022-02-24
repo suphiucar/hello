f3a98afa504ce5af32fde4e75b8f41ee095d300e
Suphi UCAR <suphiucar@gmail.com>
2022-02-24 13:42:09 +0300

    #deneme

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ import numpy as np
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()                   # Display the plot
+
+
+#deneme
